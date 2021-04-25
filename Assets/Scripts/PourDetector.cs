3eb85166bfba0cd482b1a41c65ce5e8aa6f2f34e
Om Sinha <om_s@mt.iitr.ac.in>
2021-04-25 20:27:29 +0530

    Unity Anim

--- Assets/Scripts/PourDetector.cs
+++ Assets/Scripts/PourDetector.cs
@@ -3,5 +3,58 @@ using UnityEngine;
 
 public class PourDetector : MonoBehaviour
 {
+    public int pourThreshold = 45;
+    public Transform origin = null;
+    public GameObject streamPrefab;
 
+    public Obi.ObiEmitter s;
+
+    private bool isPouring = false;
+    private Stream currentStream = null;
+
+    private void Update()
+    {
+        bool pourCheck = CalculatePourAngle() < pourThreshold;
+
+        if(isPouring != pourCheck)
+        {
+            isPouring = pourCheck;
+
+            if (isPouring)
+            {
+                s.enabled = true;
+                //StartPour();
+            }
+            else
+            {
+                s.enabled = false;
+                //EndPour();
+            }
+        }
+    }
+
+    private void StartPour()
+    {
+        //print("Start");
+        currentStream = CreateStream();
+        currentStream.Begin();
+    }
+
+    private void EndPour()
+    {
+        //print("End");
+        currentStream.End();
+        currentStream = null;
+    }
+
+    private float CalculatePourAngle()
+    {
+        return transform.forward.y * Mathf.Rad2Deg;
+    }
+
+    private Stream CreateStream()
+    {
+        GameObject streamObject = Instantiate(streamPrefab, origin.position, Quaternion.identity, transform);
+        return streamObject.GetComponent<Stream>();
+    }
 }
